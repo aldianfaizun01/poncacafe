4131ceca65a931e18608909198fa956a922a40d6
aldianfaizun01 <m.aldianfaizunirsyad@gmail.com>
2026-01-07 15:10:22 +0700

    membuat websiter ponca cafe

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika cafe menu diklik
+document.querySelector('#cafe-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// ketik diluar sidebar untuk menghilangkan cafe-menu
+const cafe = document.querySelector('#cafe-menu');
+
+document.addEventListener('click', function(e){
+    if(!cafe.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+});
